b182fa115038ae795e58199c82c54e285101d2be
intgence <palosibasis2000@outlook.com>
2023-12-17 11:02:26 +0000

    Added an API and created the frontend

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='template')
+
+JOBS = [{
+    'id': 1,
+    'title': 'Data Analyst',
+    'Location': 'Bengaluru, India',
+    'Salary': 'Rs. 10,00,000'
+}, {
+    'id': 2,
+    'title': 'Data Engineer',
+    'Location': 'Bengaluru, India',
+    'Salary': 'Rs. 15,00,000'
+}, {
+    'id': 3,
+    'title': 'Full Stack Developer',
+    'Location': 'Delhi, India',
+    'Salary': 'Rs. 10,00,000'
+}]
+
 
 @app.route('/')
 def hello_world():
-  return 'Hello, World!'
+  return render_template('index.html', jobs=JOBS)
+
+@app.route('/jobs')
+def list_of_jobs():
+  return jsonify(JOBS)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
